3c080c83000e0aed6ccc2c53c2a988c2ec6fa52e
Ranzor234 <49786512+Ranzor234@users.noreply.github.com>
2020-07-09 23:50:09 +0700

    Text-edit

--- index.js
+++ index.js
@@ -6,16 +6,28 @@
 
 // slider.addEventListener('click', changeTheme())
 
+const slider = document.querySelector(".slider");
+
 function setTheme(themeName) {
-    localStorage.setItem('theme', themeName);
-    document.documentElement.className = themeName;
+  localStorage.setItem("theme", themeName);
+  document.documentElement.className = themeName;
 }
 
 // function to toggle between light and dark theme
 function toggleTheme() {
-    if (localStorage.getItem('theme') === 'blacktheme') {
-        setTheme('whitetheme');
-    } else {
-        setTheme('blacktheme');
-    }
+  if (localStorage.getItem("theme") === "blacktheme") {
+    setTheme("whitetheme");
+  } else {
+    setTheme("blacktheme");
+  }
 }
+
+(function() {
+  if (localStorage.getItem("theme") === "theme-dark") {
+    setTheme("theme-dark");
+  } else {
+    setTheme("theme-light");
+  }
+})();
+
+slider.addEventListener("click", toggleTheme());
